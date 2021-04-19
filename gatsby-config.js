4a088fcdfd8e97226e87ded91e6332c2ef16aba3
Ahmed Hany <dev.ahmedhany@gmail.com>
2021-04-19 16:26:46 +0200

    mdx

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,17 +20,19 @@ module.exports = {
       },
     },
     {
-      resolve: "gatsby-plugin-page-creator",
+      resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/content/posts`,
+        name: `posts`,
+        path: `./content/posts/`,
       },
     },
     {
-      resolve: `gatsby-plugin-mdx`,
+      resolve: "gatsby-plugin-page-creator",
       options: {
-        extensions: [`.mdx`, `.md`],
+        path: `./content/posts`,
       },
     },
+    'gatsby-plugin-mdx',
     {
       resolve: `gatsby-transformer-remark`,
       options: {
@@ -46,7 +48,7 @@ module.exports = {
               // you may use this to prevent Prism from re-processing syntax.
               // This is an uncommon use-case though;
               // If you're unsure, it's best to use the default value.
-              classPrefix: "language-",
+              classPrefix: "language-js",
               // This is used to allow setting a language for inline code
               // (i.e. single backticks) by creating a separator.
               // This separator is a string and will do no white-space
@@ -66,7 +68,7 @@ module.exports = {
               // Defaults to false.
               // If you wish to only show line numbers on certain code blocks,
               // leave false and use the {numberLines: true} syntax below
-              showLineNumbers: false,
+              showLineNumbers: true,
               // If setting this to true, the parser won't handle and highlight inline
               // code used in markdown i.e. single backtick code like `this`.
               noInlineHighlight: false,
@@ -121,6 +123,6 @@ module.exports = {
         path: "./src/pages/",
       },
       __key: "pages",
-    },
+    }
   ],
 };
